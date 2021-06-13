b47c767bb9caaeb47ecfaf191c3a089802062128
lixinxiang <lixinxiang@vv.cn>
2021-06-13 20:04:04 +0800

    新增基础布局

---
+++ src/layouts/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-header class="header">
+    <el-dropdown>
+      <div>
+        <i class="el-icon-setting"></i>
+        <span style="margin-left: 10px">王小虎</span>
+      </div>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>查看</el-dropdown-item>
+        <el-dropdown-item>新增</el-dropdown-item>
+        <el-dropdown-item>删除</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </el-header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 100%;
+  background: #eee;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+</style>
---
+++ src/layouts/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <sidebar />
+    <el-container direction="vertical">
+      <navbar />
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style></style>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Sidebar from "./components/Sidebar";
+
+export default {
+  components: {
+    Sidebar,
+    Navbar,
+  },
+  data() {},
+};
+</script>
